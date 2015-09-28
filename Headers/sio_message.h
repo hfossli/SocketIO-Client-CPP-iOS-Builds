8828d02f12d1bd256f9d6c38aa4ec0be3b524bc2
Håvard Fossli <hfossli@gmail.com>
2015-09-28 12:32:09 +0200

    1.6.1

--- Headers/sio_message.h
+++ Headers/sio_message.h
@@ -27,7 +27,8 @@ namespace sio
             flag_binary,
             flag_array,
             flag_object,
-			flag_boolean
+			flag_boolean,
+			flag_null
         };
 
 		virtual ~message(){};
@@ -112,6 +113,21 @@ namespace sio
     protected:
         message(flag f):_flag(f){}
     };
+
+	class null_message : public message
+	{
+	protected:
+        null_message()
+			:message(flag_null)
+        {
+        }
+
+	public:
+        static message::ptr create()
+        {
+            return ptr(new null_message());
+        }
+	};
 
 	class bool_message : public message
 	{
@@ -293,6 +309,12 @@ namespace sio
 
         }
 
+        list & operator= (const message::list && rhs)
+        {
+            m_vector = std::move(rhs.m_vector);
+            return *this;
+        }
+
 		template <typename T>
 		list(T&& content,
 			typename enable_if<is_same<vector<message::ptr>,typename remove_reference<T>::type>::value>::type* = 0):
@@ -365,6 +387,13 @@ namespace sio
         {
             message::ptr arr = array_message::create();
             arr->get_vector().push_back(string_message::create(event_name));
+            arr->get_vector().insert(arr->get_vector().end(),m_vector.begin(),m_vector.end());
+            return arr;
+        }
+
+        message::ptr to_array_message() const
+        {
+            message::ptr arr = array_message::create();
             arr->get_vector().insert(arr->get_vector().end(),m_vector.begin(),m_vector.end());
             return arr;
         }
